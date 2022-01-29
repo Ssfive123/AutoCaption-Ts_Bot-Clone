7ca09d3df6edd4fa7aaa7e28e3487acebd43f865
anandhu <84675486+Anandhu123555@users.noreply.github.com>
2022-01-29 20:19:35 +0530

    Update autoedit.py

--- Plugins/autoedit.py
+++ Plugins/autoedit.py
@@ -43,6 +43,9 @@ async def editing(bot, message):
                  message_id = message.message_id,
                  caption = file_caption + "\n \n" + "**♻️ᴊᴏɪɴ @Cinemahub182**",
                  parse_mode = "markdown"
+                 reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton("⚡ᴊᴏɪɴ ɢʀᴏᴜᴘ⚡", url="https://t.me/Cinemahub182"),
+                                              InlineKeyboardButton("💥ꜱʜᴀʀᴇ💥", url="https://t.me/share/url?url=https://t.me/Cinemahub182") ],
+                                            [ InlineKeyboardButton("🔖ᴅᴏᴡɴʟᴏᴀᴅ ꜱᴜʙᴛɪᴛʟᴇ🔖", url="https://telegra.ph/httpstelegraphfilee908cfc0d08e47ca337ecjpg-01-06") ] ] )
              )
           elif caption_position == "nil":
              await bot.edit_message_caption(
