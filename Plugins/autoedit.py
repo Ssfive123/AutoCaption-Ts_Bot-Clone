b550a8c65565cd2cd124468823ea1ba1521af3a2
anandhu <84675486+Anandhu123555@users.noreply.github.com>
2022-04-11 00:32:25 +0530

    Update autoedit.py

--- Plugins/autoedit.py
+++ Plugins/autoedit.py
@@ -46,7 +46,7 @@ async def editing(bot, message):
                  parse_mode = "markdown",
                  reply_markup=InlineKeyboardMarkup(
               [[
-              InlineKeyboardButton("❤️‍🔥 ᴊᴏɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ ❤️‍🔥", url="https://t.me/+2HojCKedvfM2M2U1")
+              InlineKeyboardButton("❤️‍🔥 ᴊᴏɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ ❤️‍🔥", url="https://t.me/+tsGRU0kdPUs2Y2U9")
               ]]
         )
                  )
